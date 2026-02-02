4503d54fcba5e759a253bc135287a7b6be3a46af
hali <hali@ksztone.com>
2026-02-02 10:51:12 +0800

    feat: 优化输出

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { ConfigEnv, defineConfig, loadEnv, ProxyOptions, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
-
+const outDir = path.resolve(__dirname, '../google_aistudio_preview/playground-dist');
 const proxy = (target: string) => {
   const ProxyList: Record<string, string | ProxyOptions> = {
     '/api': {
@@ -19,6 +20,10 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig=>{
   const env = loadEnv(mode, root)
   const {  VITE_APP_PORT, VITE_APP_PROXY } = env
   return {
+    build: {
+      outDir: isBuild ? outDir : 'dist',
+      sourcemap: false,
+    },
     plugins: [react()],
     optimizeDeps: {
       exclude: ['esbuild-wasm']
